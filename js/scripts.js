806400af88567c5862c7da8820d4932b35483487
khalifa2003 <132180439+khalifa2003@users.noreply.github.com>
2024-10-28 17:54:23 +0300

    Update navbar button icons

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,21 @@
 // Toggle dropdown visibility
 
-function toggleDropdown() {
+function toggleImage() {
   if (event.target.matches(".profile-pic")) {
-    document.getElementById("dropdownMenu").classList.toggle("active");
+    document.getElementById("my-dropdownMenu").classList.toggle("active");
+  }
+}
+function toggleDropdown(dropdownId) {
+  const dropdown = document.getElementById(dropdownId);
+  const isActive = dropdown.classList.contains("active");
+
+  // Close any open dropdowns
+  document.querySelectorAll(".dropdown-menu").forEach((menu) => {
+    menu.classList.remove("active");
+  });
+
+  // Toggle the selected dropdown
+  if (!isActive) {
+    dropdown.classList.add("active");
   }
 }
